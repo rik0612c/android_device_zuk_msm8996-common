1d2f621078ef5c7cd0c3a1902145e72ae1a0f08e
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    msm8996-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- msm8996.mk
+++ msm8996.mk
@@ -32,6 +32,9 @@ $(call inherit-product, vendor/zuk/msm8996-common/msm8996-common-vendor.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# This is required to build applypatch for updating recovery
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
