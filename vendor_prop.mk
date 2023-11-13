05cf44b48969e9d9df5b4eb4bf607f6a8fa1e369
Bruno Martins <bgcngm@gmail.com>
2023-11-13 13:48:00 +0000

    msm8996-common: Move to QTI health AIDL service
    The health AIDL HAL service provides functionalities of charger,
    therefore system charger at /system/bin/charger is deprecated.
    On top of that, QTI health AIDL HAL service enables suspend by
    default, the equivalent of setting ro.charger.enable_suspend
    for legacy charger.
    
    Change-Id: I59c23e7974cea1174b0161f31a535fa3afa1e5c9

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -67,8 +67,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.charger.disable_init_blank=true \
-    ro.charger.enable_suspend=true
+    ro.charger.disable_init_blank=true
 
 # Chipset
 PRODUCT_PRODUCT_PROPERTIES += \
